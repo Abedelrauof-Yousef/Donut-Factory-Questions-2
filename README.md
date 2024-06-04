45e0ec44be2dd50114b95e9381f28302dc55e09b
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-04 22:46:48 +0300

    Questions part 2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Donut-Factory-Questions-2
+
+
+live (url): https://abedelrauof-yousef.github.io/Donut-Factory-Questions-2/
---
+++ app.js
@@ -0,0 +1,42 @@
+// Q7
+
+// for (let i = 0; i < 6; i++ ){
+//     alert(i);
+// }
+
+
+
+
+// Q8
+
+// let str = "";
+// for (let i = 0; i < 6; i++){
+//     str+= i;
+// }
+// alert(str);
+
+    
+
+
+// Q9
+
+// for (let i = 0; i < 20; i += 3){
+//     alert(i);
+// }
+
+
+
+
+// Q10
+
+// let pickNumber = prompt("Pick a number in the range 0 - 100");
+
+// while (pickNumber < 0 || pickNumber > 100) {
+//     pickNumber = prompt("Error, Please pick a number in the range 0 - 100");
+// }
+
+// alert("You picked: " + pickNumber);
+
+
+
+
